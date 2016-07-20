0cca32780d42c7167d600bfbcd5db7c720b25a25
hm.lee <tukbbae@gmail.com>
2016-07-20 10:42:26 +0900

    Contacts util 추가

---
+++ app/src/main/java/leesc/chatchat/utils/CharUtils.java
@@ -0,0 +1,20 @@
+package leesc.chatchat.utils;
+
+import android.text.TextUtils;
+
+import com.mogua.localization.KoreanChar;
+
+public class CharUtils {
+
+    public static String getInitialChar(String str) {
+    	if (TextUtils.isEmpty(str))
+    	{
+    		return "";
+    	}
+        char initialChar = Character.toUpperCase(str.charAt(0));
+        if (KoreanChar.isSyllable(initialChar)) {
+            initialChar = KoreanChar.getCompatibilityChoseong(initialChar);
+        }
+        return Character.toString(initialChar);
+    }
+}
